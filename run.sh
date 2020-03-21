ffab292d097477600eaeca3cc6f5f3634cfeb336
YurkoFlisk <yurko.assimo@gmail.com>
2020-03-21 17:03:12 +0200

    Added debug information

--- run.sh
+++ run.sh
@@ -23,6 +23,9 @@ for possibleExt in ${possibleExts[@]}; do
 done
 echo "Supported cpu extensions: ${cpuExts[*]}" >> $logFile
 
+echo "Log index: $1"
+echo "Number of repetitions: $2"
+
 calcResAndTime() {
 	local lExecTime=`( time for i in {1..$2}; do ./$exeFile < $inputFile > $tmpFile; done ) 2>&1 | grep real | awk '{print $2}'`
 	local lExecRes=`cat $tmpFile | awk '{sum=(sum+$1)%1000}END{print sum}'`
